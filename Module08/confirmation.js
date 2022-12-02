a3ead674aec3aceb9775e7d655e3e7df9dff08d5
Marcuszz <102037920+Marcuszz@users.noreply.github.com>
2022-12-02 16:15:26 +0800

    Add files via upload

---
+++ Module08/confirmation.js
@@ -0,0 +1,23 @@
+function confirm(){
+    if(document.empForm.empid.value==""){
+        document.getElementById("empidErr").innerHTML = "Please enter employee ID";
+        return false;
+    }
+
+    if(document.empForm.lname.value==""){
+        document.getElementById("lnameErr").innerHTML = "Please enter your last name";
+        return false;
+    }
+
+    if(document.empForm.email.value==""){
+        document.getElementById("emailErr").innerHTML = "Please enter your email";
+        return false;
+    } else {
+        var regex = /^\$+@\S+\.\S+$/;
+        if(regex.test(document.empForm.email.value)==false){
+            alert("Please enter a valid email address");
+            return false;
+        }
+    }
+}
+
